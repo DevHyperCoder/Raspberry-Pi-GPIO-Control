b1dfdade5db963d4ebeaa6838de087e2ac8bb32d
DevHyperCoder <46173722+DevHyperCoder@users.noreply.github.com>
2020-08-12 07:07:21 +0530

    Added documentation

--- led.py
+++ led.py
@@ -1,6 +1,11 @@
 from gpiozero import LED
 from time import sleep
+
+# Initialise the LED @ Pin 14
 led=LED(14)
+
+# Infinte loop
+# Turn the LED ON and OFF with a 1 second delay
 while True:
     led.on()
     sleep(1)
